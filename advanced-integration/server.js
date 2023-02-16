8ba29713f27b715fa30a0ec27599cb8bceb53bf9
Jesse Shawl <jshawl@paypal.com>
2023-02-16 14:17:51 -0600

    log URL with port on app start

--- advanced-integration/server.js
+++ advanced-integration/server.js
@@ -38,4 +38,6 @@ app.post("/api/orders/:orderID/capture", async (req, res) => {
   }
 });
 
-app.listen(8888);
+app.listen(8888, () => {
+  console.log('App listening at http://localhost:8888/')
+});
