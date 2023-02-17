b15a93633d580ebd599812c285129ec435c3f0e3
Jesse Shawl <jshawl@paypal.com>
2023-02-17 09:55:54 -0600

    chore: log URL with port on app start (#26)

--- standard-integration/server.js
+++ standard-integration/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config"; // loads variables from .env file
 import express from "express";
 import * as paypal from "./paypal-api.js";
+const {PORT = 8888} = process.env;
 
 const app = express();
 
@@ -28,4 +29,6 @@ app.post("/my-server/capture-paypal-order", async (req, res) => {
   }
 });
 
-app.listen(8888);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
