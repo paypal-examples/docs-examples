cc2dfb34457a2d07e46005e1be58e38587d797f7
lordlawy11 <144285647+lordlawy11@users.noreply.github.com>
2024-03-06 04:24:38 -0800

    Update server.js

--- standard-integration/server/server.js
+++ standard-integration/server/server.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import "dotenv/config";
 import path from "path";
 
-const { Afzu7yKcvnYALspKP3IZKVi89S2qNs6zJHPARYTgHbmKuk04cHqOVEEsSV8Ejrp2CczBstP2lsdrMJIt, ENy5TmjQJ714xczrhjkiYPOdvivbyXDnbf3Xiz75083Up6a9YxQxPwW8OBwiSVQa5h5DDR7danB7F4W2, PORT = 8888 } = process.env;
+const { AS_-NlrYn0Y4j95hpfDdOWta4QaSghsKqaiFFBdIazZB_1IQyMwN6MLOegrg8RyaGWhM87kufkMX9hkD, ENy5TmjQJ714xczrhjkiYPOdvivbyXDnbf3Xiz75083Up6a9YxQxPwW8OBwiSVQa5h5DDR7danB7F4W2, PORT = 8888 } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
